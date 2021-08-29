2643ddcfc99ba0ea38e506222bc836acc97212b4
Carlos C Soto <eclipxe13@gmail.com>
2021-08-28 21:59:57 -0500

    Add controller to build cfdi from json

---
+++ app/Controllers/BuildCfdiFromJsonController.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use Dufrei\ApiJsonCfdiBridge\Factory;
+use Dufrei\ApiJsonCfdiBridge\Values\CredentialCsd;
+use Dufrei\ApiJsonCfdiBridge\Values\JsonContent;
+use PhpCfdi\Credentials\Credential;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Psr7\Factory\StreamFactory;
+
+final class BuildCfdiFromJsonController
+{
+    public function __construct(
+        private Factory $actionFactory,
+        private StreamFactory $streamFactory,
+    ) {
+    }
+
+    public function __invoke(Request $request, Response $response): Response
+    {
+        $inputs = $request->getParsedBody();
+
+        $json = new JsonContent(strval($inputs['json'] ?? ''));
+        $credential = Credential::create(
+            strval($inputs['certificate'] ?? ''),
+            strval($inputs['privatekey'] ?? ''),
+            strval($inputs['passphrase'] ?? ''),
+        );
+        $csd = new CredentialCsd($credential);
+
+        $action = $this->actionFactory->createBuildCfdiFromJsonAction();
+        $result = $action->execute($json, $csd);
+        $responseData = [
+            'converted' => $result->getConvertedXml(),
+            'sourcestrin' => $result->getPreCfdi()->getSourceString(),
+            'precfdi' => $result->getPreCfdi()->getXml(),
+            'uuid' => $result->getCfdi()->getUuid(),
+            'xml' => $result->getCfdi()->getXml(),
+        ];
+
+        $responseBody = $this->streamFactory->createStream(json_encode($responseData));
+        return $response
+            ->withStatus(200)
+            ->withHeader('Content-Type', 'application/json')
+            ->withBody($responseBody);
+    }
+}
--- public/index.php
+++ public/index.php
@@ -2,17 +2,13 @@
 
 declare(strict_types=1);
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Controllers\BuildCfdiFromJsonController;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
+$app->post('/build-cfdi-from-json', BuildCfdiFromJsonController::class);
 
 $app->run();
