87af9fa06fe7a41e8c7d2316bf096fa7712d04e0
Carlos C Soto <eclipxe13@gmail.com>
2021-08-30 02:39:43 -0500

    Fix script to run from command line

--- bin/create-authorization-token.php
+++ bin/create-authorization-token.php
@@ -1,13 +1,16 @@
+#!/usr/bin/env php
 <?php
 
 declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
 
 use Dufrei\ApiJsonCfdiBridge\Values\Token;
 
 $token = Token::createRandom();
 
 echo implode(PHP_EOL, [
-    "Set up the environment with AUTHORIZATION_KEY={$token->getHash()}",
+    "Set up the environment with AUTHORIZATION_TOKEN='{$token->getHash()}'",
     'Your client must use the HTTP authorization header:',
     "   Authorization: Bearer {$token->getToken()}",
     '',
